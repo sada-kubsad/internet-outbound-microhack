e54fba4fd42e7cb86ef99a02a44a5caed42bb07e
Federico Guerrini <fguerri@microsoft.com>
2021-01-09 23:08:59 +0100

    Updated

--- scripts/wvd-firewall-rules.ps1
+++ scripts/wvd-firewall-rules.ps1
@@ -22,18 +22,20 @@ foreach ($url in $appRulesUrls) {
 
     $appRules += new-AzFirewallApplicationRule -Name "wvd_app_rule_$index" -SourceAddress $wvdSessionHostPoolSubnet -TargetFqdn $fqdn -Protocol $port -Description "Allow access to $fqdn"
     $index += 1
+    Write-Host "Created application rule for FQDN: $fqdn"
          
 }
-
 $wvdApplicationRuleCollection = New-AzFirewallApplicationRuleCollection -Name WVD-DIRECT-ApplicationRules -Priority 1157 -ActionType Allow -Rule $appRules
 
 $netRules = @()
 $netRules += new-AzFirewallNetworkRule -Name "wvd_net_rule_1" -SourceAddress $wvdSessionHostPoolSubnet -DestinationAddress "WindowsVirtualDesktop" -DestinationPort 443 -Protocol TCP -Description "Allow access to WindowsVirtualDesktop service tag"
-    $index += 1
+Write-Host "Created network rule for Service Tag: WindowsVirtualDesktop"
 $wvdNetworkRuleCollection = New-AzFirewallNetworkRuleCollection -Name WVD-DIRECT-NetworkRules -Priority 1157 -ActionType Allow -Rule $netRules
 
-
+Write-Host "Adding rules to firewall config..."
 $azfw = Get-AzFirewall -Name $azFwName -ResourceGroupName $rg
 $azfw.ApplicationRuleCollections.Add($wvdApplicationRuleCollection)
 $azfw.NetworkRuleCollections.Add($wvdNetworkRuleCollection)
+Write-Host "Updating firewall..."
 Set-AzFirewall -AzureFirewall $azfw
+Write-Host "Done!"
